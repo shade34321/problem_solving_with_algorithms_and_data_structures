439551d3efaf0981692a8fa38b70bbfd86ccfbaf
Shade Alabsa <shade34321@gmail.com>
2015-05-26 13:30:18 -0400

    Took the node class out of unordered_list.py and put it into it's own file. This way I can use it across multiple data structures.

---
+++ node.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+class Node:
+    
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, newdata):
+        self.data = newdata
+
+    def setNext(self, next_node):
+        self.next = next_node
---
+++ ordered_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+class OrderedList:
+
+    def __init__(self):
+        self.head = None
+
+    def add(self, item):
+
+    def remove(self, item):
+
+    def search(self, item):
+
+    def isEmpty(self):
+
+    def size(self):
+
+    def index(self, item):
+
+    def pop(self):
+
+    def pop(self, position):
